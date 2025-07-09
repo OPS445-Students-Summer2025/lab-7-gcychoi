a8e279da1a27ef9493aaa2168167cb7d8fbd71a2
Chung Yin Choi <cychoi@myseneca.ca>
2025-07-09 19:50:59 -0400

    Added lab7a

--- lab7a.py
+++ lab7a.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-# Student ID: [seneca_id]
+# Name: lab7a.py
+# Author: Chung Yin Choi
+# Author ID: cychoi
+# Date Created: 2025/07/09
+
 class Time:
     """Simple object type for time of the day.
     data attributes: hour, minute, second
@@ -20,6 +24,15 @@ def sum_times(t1, t2):
     sum.hour = t1.hour + t2.hour
     sum.minute = t1.minute + t2.minute
     sum.second = t1.second + t2.second
+
+    if sum.second >= 60:
+        sum.second = sum.second - 60
+        sum.minute = sum.minute + 1
+
+    if sum.minute >= 60:
+        sum.minute = sum.minute - 60
+        sum.hour = sum.hour + 1
+
     return sum
 
 def valid_time(t):
